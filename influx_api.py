736e2ee8ab552da9e5b5d32fc6e9d3a1f1252acd
Oleksandr Naumets <a.naumets111@gmail.com>
2019-04-14 02:19:08 +0300

    Update description for InfluxDB class

--- influx_api.py
+++ influx_api.py
@@ -1,10 +1,15 @@
 class InfluxWrapper(object):
+    """InfluxDB wrapper"""
+    
     def __init__(self):
+        """Add description"""
         pass
 
     def init(self):
+        """Add description"""
         pass
 
     def write(self, data):
+        """Add description"""
         pass
 
